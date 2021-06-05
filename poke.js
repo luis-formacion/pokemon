e5c4ddaa9df47e539416dd4795efcdc3f8e4f8c2
luis-formacion <84958087+luis-formacion@users.noreply.github.com>
2021-06-05 10:07:11 +0200

    Add files via upload

---
+++ poke.js
@@ -0,0 +1,62 @@
+/* -------------- IDs --------------------*/
+let mas = document.getElementById("mas");
+let menos = document.getElementById("menos");
+let identificador = document.getElementById("identificador");
+
+// Inicio --------------------------------------
+identificador.innerHTML=1;
+actualiza();
+
+
+// FUNCION INC ------------------------
+function inc(){
+    identificador.innerHTML++;
+    actualiza();
+}
+// FUNCION DEC ------------------------
+function dec() {
+    if (identificador.innerHTML>1){
+        identificador.innerHTML--;
+        actualiza();
+    }
+}
+
+// FUNCION ACTUALIZA------------------------------------
+
+function actualiza() {
+    
+let contador = identificador.innerHTML;
+url =  "https://pokeapi.co/api/v2/pokemon/" + contador + "/";
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+
+let nombre = document.getElementById("nombre");
+nombre.innerHTML=`${data.nombre}`;
+
+let frente = document.getElementById("frente");
+let trasero = document.getElementById("trasero");
+
+frente.innerHTML=`
+<img src="${data.sprites.front_default}" 
+onMouseOver="src='${data.sprites.front_shiny}';"
+onMouseOut= "src='${data.sprites.front_default}';">
+`
+
+trasero.innerHTML=`
+<img src="${data.sprites.back_default}" 
+onMouseOver="src='${data.sprites.back_shiny}';"
+onMouseOut= "src='${data.sprites.back_default}';">
+`
+})
+.catch(err => console.log(err));
+}
+
+
+
+
+
+
+
+
